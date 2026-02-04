2466a0c5e8e85f8bad97354e3e13796c6644dba8
Serhii <fserg2003@gmail.com>
2026-02-04 16:41:40 +0200

    Initial commit

---
+++ test/specs/index.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+
+const assert = chai.assert;
+const expect = chai.expect;
+chai.should();
+
+describe("Chai interfaces", () => {
+
+    it("Assert", async () => {
+        await browser.url("https://www.epam.com/");
+        const title = await browser.getTitle();
+        assert.equal(title, "EPAM | Software Engineering & Product Development Services");
+    });
+
+    it("Expect", async () => {
+        await browser.url("https://www.epam.com/");
+        const title = await browser.getTitle();
+        expect(title).to.equal("EPAM | Software Engineering & Product Development Services");
+    });
+
+    it("Should", async () => {
+        await browser.url("https://www.epam.com/");
+        const title = await browser.getTitle();
+        title.should.equal("EPAM | Software Engineering & Product Development Services");
+    })
+
+});
